f0a27a97b4be44b5ed94220482a54134db2a6288
RedRain <447154278@qq.com>
2016-08-16 11:57:30 +0800

    调整目录结构

--- 日历Cal/日历Cal/ViewController.m
+++ 日历Cal/日历Cal/ViewController.m
@@ -13,7 +13,9 @@
 
 #define kScreenHeight  ([[UIScreen mainScreen] bounds].size.height)
 #define kScreenWidth   ([[UIScreen mainScreen] bounds].size.width)
+
 @interface ViewController ()<UICollectionViewDataSource,UICollectionViewDelegate>
+@property (weak, nonatomic) IBOutlet UILabel *monthLabel;
 
 @property (nonatomic, strong) RRCalLogic *RRlogic;
 @property (nonatomic, strong) UICollectionView *collectionView;
@@ -23,9 +25,10 @@ static NSString *cellID = @"cellID";
 
 @implementation ViewController
 
+#pragma mark -  Life Cycle
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    
     UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc]init];
     layout.itemSize = CGSizeMake((kScreenWidth/7), (40));
     layout.minimumInteritemSpacing = 0;
@@ -39,7 +42,7 @@ static NSString *cellID = @"cellID";
     [collectionView registerNib:[UINib nibWithNibName:@"RRCalCell" bundle:nil]
      forCellWithReuseIdentifier:cellID];
 }
-
+#pragma mark -  UICollectionViewDelegate
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
     RRCalCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:cellID forIndexPath:indexPath];
     cell.calDayModel = self.RRlogic.calendarDays[indexPath.item];
@@ -52,20 +55,39 @@ static NSString *cellID = @"cellID";
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
     return 6 * 7;
 }
-
+#pragma mark -  CustomDelegate
+#pragma mark -  Event Response
+#pragma mark -  Notification Motheds
+#pragma mark -  Button Callbacks
 - (IBAction)upMonth:(id)sender {
     [self.RRlogic goToPreviousMonth];
     [self.collectionView reloadData];
+    [self reloadMonthLabel];
 }
 - (IBAction)downMonth:(id)sender {
     
     [self.RRlogic goToNextMonth];
     [self.collectionView reloadData];
+    [self reloadMonthLabel];
 }
+- (IBAction)returnCurrentMonth:(id)sender {
+    [self.RRlogic loadCurrentDate];
+    [self.collectionView reloadData];
+    [self reloadMonthLabel];
+}
+
+#pragma mark -  Private Methods
+
+- (void)reloadMonthLabel{
+    self.monthLabel.text = [NSString stringWithFormat:@"%lu年%lu月", (unsigned long)self.RRlogic.selectedCalendarDay.year, (unsigned long)self.RRlogic.selectedCalendarDay.month];
+}
+#pragma mark -  Public Methods
+#pragma mark -  Getters and Getters
 
 - (RRCalLogic *)RRlogic{
     if (!_RRlogic) {
         _RRlogic = [[RRCalLogic alloc]init];
+        [self reloadMonthLabel];
     }
     return _RRlogic;
 }
